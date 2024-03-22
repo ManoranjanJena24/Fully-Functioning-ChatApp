bddeaf633ff86996effcb0aa6fc3c706f4eb9a2d
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-03-23 00:42:08 +0530

    Rendering Messages on FrontEnd

--- client/chat.js
+++ client/chat.js
@@ -1,22 +1,34 @@
 let url = "http://localhost:3000"
+let username
 
 let token;
+function logout() {
+    console.log(token)
+    axios.get(`${url}/user/logout`, { headers: { "Authorization": token } }).then((res) => {
+        console.log(res)
+        localStorage.clear()
+        alert(res.data)
+        window.location.href = 'login.html'
+    }).catch(err => console.log(err))
+}
+
+function getLoggedInUsers() {
+    axios.get(`${url}/user/online`).then((res) => {
+        console.log(res.data.users)
+    })
+}
 
 document.getElementById('messageForm').addEventListener('submit', function (event) {
     event.preventDefault();
-   
-
     const messageData = {
         message: document.getElementById('messageInput').value,
-        
     };
-
     this.reset();
     sendMessage(messageData);
 });
 
 function sendMessage(data) {
-    axios.post(`${url}/message/add-message`,data, { headers: { "Authorization": token } }).then((res) => {
+    axios.post(`${url}/message/add-message`, data, { headers: { "Authorization": token } }).then((res) => {
         console.log(res)
         getMessages()
     }).catch((err) => {
@@ -26,7 +38,7 @@ function sendMessage(data) {
 
 function getMessages() {
     axios.get(`${url}/message/get-messages`).then((res) => {
-        console.log(res.data)
+        // console.log(res.data)
         renderMessages(res.data)
     }).catch((err) => {
         console.log(err)
@@ -34,62 +46,33 @@ function getMessages() {
 }
 
 function renderMessages(data) {
-    // TODO
-    
+    console.log(data)
+    data.forEach(message => {
+        displayMessage(message.message, message.name, message.name === username)
+    });
 }
 
+function displayMessage(message, name, isUserMessage) {
+    const chatContainer = document.getElementById('chat-container');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
 
-
-// function checkPremium(value) {
-
-//     const button = document.getElementById('razoorpay-button');
-//     const text = document.getElementById('premium-text')
-//     const leaderBoard = document.getElementById("leaderBoard-button")
-//     const salaryForm = document.getElementById('salary-form')
-//     // console.log(token)
-//     const reportBtn = document.getElementById('report-button');
-//     if (!value) {
-//         button.style.display = "inline-block";  //make the button visible
-//         text.style.display = "none";
-//         leaderBoard.style.display = "none"
-//         salaryForm.style.display = "none"
-//         reportBtn.style.display = "none"
-//     }
-//     else {
-//         button.style.display = "none";  //Hide the button
-//         text.style.display = "inline-block";
-//         leaderBoard.style.display = "inline-block"
-//         salaryForm.style.display = "inline-block"
-//         reportBtn.style.display = "inline-block"
-
-//     }
-// }
-
-function logout() {
-    console.log(token)
-    axios.get(`${url}/user/logout`, { headers: { "Authorization": token } }).then((res) => {
-        console.log(res)
-        localStorage.clear()
-        alert(res.data)
-        window.location.href = 'login.html'
-    }).catch(err => console.log(err))
-
-
+    if (isUserMessage) {
+        messageElement.classList.add('user-message');
+        messageElement.innerHTML = `<span class="username">${name}</span><div class="user-message-content">${message}</div>`;
+    } else {
+        messageElement.classList.add('other-message');
+        messageElement.innerHTML = `<span class="name">${name}</span><div class="message-content">${message}</div>`;
+    }
+    chatContainer.appendChild(messageElement);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 }
-
-function getLoggedInUsers() {
-
-    axios.get(`${url}/user/online`).then((res) => {
-        console.log(res.data.users)
-    })
-}
-
-
-
 window.addEventListener('DOMContentLoaded', () => {
     token = localStorage.getItem('token')
+    username = localStorage.getItem('user')
+    const welcome = document.getElementById('welcome')
+    welcome.innerHTML = `Start Messaging, ${username}`
     // checkPremium(localStorage.getItem('isPremium') === 'true')
-    getLoggedInUsers();
-
-
+    getLoggedInUsers()
+    getMessages()
 });
